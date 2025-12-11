999ad8479e9f9f20a7ff0184e706d880698d3c46
Steeven Maila <steevenandresmaila@gmail.com>
2025-12-11 12:56:58 -0500

    agregar depdencias y presentacion con login

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,36 @@ import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { SearchProvider } from './context/SearchContext'
 import EstadoCuenta from './pages/EstadoCuenta'
+import MainWrapper from './layouts/MainWrapper'
+import PrivateRoute from './layouts/PrivateRoute'
+import Login from './pages/Login'
 
 function App() {
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<div>Este es elñ inicio</div>} />
-        <Route path="/login" element={<div>inison de sesion</div>} />
-        <Route 
-          path="/estadoCuenta" 
-          element={
-            <SearchProvider>
-              <EstadoCuenta/>
-            </SearchProvider>
-          } />
-      </Routes>
+      <MainWrapper>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <SearchProvider>
+                  <EstadoCuenta />
+                </SearchProvider>
+              </PrivateRoute>
+            }
+          />
+          <Route path="/login" element={<Login/>} />
+          <Route 
+            path="/estadoCuenta" 
+            element={
+              <SearchProvider>
+                <EstadoCuenta/>
+              </SearchProvider>
+            } />
+        </Routes>
+      </MainWrapper>
     </BrowserRouter>
   )
 }
